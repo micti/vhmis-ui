09bcb7d7a342b6eef4fa02a942d476eafc28683b
Nhật Anh <lenhatanh86@gmail.com>
2018-06-06 09:43:46 +0700

    Sửa lại tên class ở Page Menu

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,9 @@
 // Element.classList.toggle()
 
 document.addEventListener('DOMContentLoaded', function () {
-  var appsOpenIcon = document.querySelector('.page-menu--apps-icon.open-button')
-  var appsCloseIcon = document.querySelector('.page-menu--apps-icon.close-button')
-  var appsMenu = document.querySelector('.apps-menu')
+  var appsOpenIcon = document.querySelector('.extra-menu--apps-icon.open-button')
+  var appsCloseIcon = document.querySelector('.extra-menu--apps-icon.close-button')
+  var appsMenu = document.querySelector('.page-menu')
   appsOpenIcon.addEventListener('click', function (e) {
     appsMenu.classList.toggle('is-active')
     appsOpenIcon.classList.toggle('is-active')
